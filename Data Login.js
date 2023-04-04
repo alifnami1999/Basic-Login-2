d3185d546e8c4d99434f28470b6213cdab3a9a5b
alifnami1999 <alifnami1999@gmail.com>
2023-04-04 15:29:50 +0800

    basic login and register

---
+++ Data Login.js
@@ -0,0 +1,22 @@
+let dbUsers = [
+    { 
+       Username:"Soo",
+       Password:"12",
+       Name:"Soo",
+       Email:"Soo@utem.edu.my"
+    },
+    {
+       Username:"John",
+       Password:"34",
+       Name:"John",
+       Email:"John@utem.edu.my"
+    },
+    {
+        Username:"Ali",
+        Password:"56",
+        Name:"Ali",
+        Email:"Ali@utem.edu.my"
+    }
+]
+
+
